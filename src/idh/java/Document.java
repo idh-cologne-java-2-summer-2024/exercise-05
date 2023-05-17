6371bf4cd5a6429d0eda689f66f72f703ddb9bf5
Nils Reiter <nils.reiter@uni-koeln.de>
2023-05-17 12:19:42 +0200

    solution in class

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -3,8 +3,12 @@ package idh.java;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
 
-public class Document {
+public class Document implements Iterable<String> {
 	String documentText;
 
 	public static Document readFromFile(File f) throws IOException {
@@ -31,6 +35,39 @@ public class Document {
 	
 	public static final void main(String[] args) throws IOException {
 		Document d = Document.readFromFile(new File("data/dracula.txt"));
+		for(String token : d) {
+			System.out.println(token);
+		}
+	}
+
+	@Override
+	public Iterator<String> iterator() {
+		StringTokenizer st = new StringTokenizer(documentText);
+		List<String> list = new LinkedList<String>();
+		while(st.hasMoreTokens()) {
+			list.add(st.nextToken());
+		}
+		return list.iterator();
+	}
+	
+	class DocumentIterator implements Iterator<String> {
+
+		StringTokenizer st;
+		public DocumentIterator(String s) {
+			 st = new StringTokenizer(s);
+
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return st.hasMoreTokens();
+		}
+
+		@Override
+		public String next() {
+			return st.nextToken();
+		}
+		
 	}
 	
 }
