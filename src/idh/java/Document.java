a37115d267a194d90b068edadc20ff2439170fa9
maral <maral@172.29.246.122>
2024-05-28 14:25:14 +0200

    Abgabe

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -1,73 +1,47 @@
-package idh.java;
-
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Document implements Iterable<String> {
-	String documentText;
+    private String content;
 
-	public static Document readFromFile(File f) throws IOException {
-		FileReader fileReader = new FileReader(f);
-		int ch;
-		StringBuilder b = new StringBuilder();
-		while( (ch = fileReader.read()) != -1 ) {
-			b.append((char) ch);
-		}
-		fileReader.close();
-		Document doc = new Document();
-		doc.documentText = b.toString();
-		
-		return doc;
-	}
-	
-	public String getDocumentText() {
-		return documentText;
-	}
+    private Document(String content) {
+        this.content = content;
+    }
 
-	public void setDocumentText(String documentText) {
-		this.documentText = documentText;
-	}
-	
-	public static final void main(String[] args) throws IOException {
-		Document d = Document.readFromFile(new File("data/dracula.txt"));
-		for(String token : d) {
-			System.out.println(token);
-		}
-	}
+    public static Document loadFromFile(String filePath) throws IOException {
+        StringBuilder contentBuilder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                contentBuilder.append(line).append("\n");
+            }
+        }
+        return new Document(contentBuilder.toString());
+    }
 
-	@Override
-	public Iterator<String> iterator() {
-		StringTokenizer st = new StringTokenizer(documentText);
-		List<String> list = new LinkedList<String>();
-		while(st.hasMoreTokens()) {
-			list.add(st.nextToken());
-		}
-		return list.iterator();
-	}
-	
-	class DocumentIterator implements Iterator<String> {
+    @Override
+    public Iterator<String> iterator() {
+        return new DocumentIterator(content);
+    }
 
-		StringTokenizer st;
-		public DocumentIterator(String s) {
-			 st = new StringTokenizer(s);
+    private static class DocumentIterator implements Iterator<String> {
+        private StringTokenizer tokenizer;
 
-		}
-		
-		@Override
-		public boolean hasNext() {
-			return st.hasMoreTokens();
-		}
+        public DocumentIterator(String content) {
+            this.tokenizer = new StringTokenizer(content);
+        }
 
-		@Override
-		public String next() {
-			return st.nextToken();
-		}
-		
-	}
-	
+        @Override
+        public boolean hasNext() {
+            return tokenizer.hasMoreTokens();
+        }
+
+        @Override
+        public String next() {
+            return tokenizer.nextToken();
+        }
+    }
 }
